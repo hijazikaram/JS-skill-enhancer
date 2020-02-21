030382c9a0cb8fef53539831ade5e2f2cca9fcd9
Karam Hijazi <khijazi@Karams-MBP.fios-router.home>
2020-02-21 18:31:14 -0500

    remove testing file and added logic

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,9 +5,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      calc: '',
+      calc: '0',
       disableAction: true,
       result: null,
+      firstNum: '',
       actions: ['-', '+', '*', '/', '=']
     };
   }
@@ -23,46 +24,51 @@ class App extends React.Component {
       result: null
     });
   }
-
   handleActionClicked = action => {
+    const { firstNum, calc } = this.state;
     if (action === '=') {
+      const finalNum = firstNum.concat(calc);
       this.setState({
-        result: eval(this.state.calc),
+        result: eval(finalNum),
         disableAction: true,
+        firstNum: '',
         calc: ''
       });
     } else {
+      const firstNum = firstNum === '' ? calc.concat(' ', action, ' ', firstNum) : this.state.firstNum.concat(' ', this.state.calc, ' ', action );
       this.setState({
-        calc: this.state.calc.concat('', action),
+        firstNum,
+        calc: '0',
         disableAction: true,
         result: null
       });
-    };
+    }
   }
 
   clearState = () => {
     this.setState({
       calc: '',
       disableAction: true,
-      result: null
+      result: null,
+      firstNum: ''
     });
   }
   render() {
     console.log(this.state);
-    const { actions } = this.state;
-    // str 12 [+,-,/,*] 01
-    // example `12 + 01` should be `12 + 1` 
-    // if 0 is is the first then user clicks another num, remove the 0
+    const { actions, result, calc, firstNum } = this.state;
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const num = numbers.map((num, i) => <button key={i} onClick={() => this.handleNumClicked(num)}>{ num } </button>);
-    const action = actions.map((action,i) => <button key={i} disabled={this.state.disableAction} onClick={() => this.handleActionClicked(action)}>{action}</button>);
+    const action = actions.map((action, i) => <button key={i} disabled={this.state.disableAction} onClick={() => this.handleActionClicked(action)}>{action}</button>);
 
     return (
       <div className="App">
         <h1>Calculator</h1>
         <div className="calc">
           <div>
-            <input type="text" value={this.state.result ? this.state.result : this.state.calc} readOnly></input>
+            <input type="text" value={firstNum} readOnly></input>
+          </div>
+          <div>
+            <input type="text" value={result ? result : calc} readOnly></input>
           </div>
           <div>
             { num }
