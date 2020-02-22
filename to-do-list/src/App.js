a6b3d8ac3e56502ce13aa96471b8df28f8e94da2
Karam Hijazi <khijazi@Karams-MacBook-Pro.local>
2020-02-22 15:44:10 -0500

    add a way to add/delete/mark as done todos

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    toDos: [],
+    toDo: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({ toDo: event.target.value });
+  }
+
+  addToDo = () => {
+    const toDos = [...this.state.toDos, this.state.toDo];
+    this.setState({
+      toDos,
+      toDo: '',
+      isDone: false
+    })
+  }
+
+  deleteToDo = (toDo) => {
+    // find the index
+    const index = this.state.toDos.indexOf(toDo);
+    // if found, remove from the array
+    if (index > -1) {
+      this.state.toDos.splice(index, 1);
+    }
+    // update the state with the new array
+    this.setState({
+      toDos: this.state.toDos
+    })
+  }
+
+  handleCheckChange = (event) => {
+    const name = event.target.name;
+    this.setState({
+      [name]: event.target.checked
+    });
+  }
+
+  render() {
+    // console.log(this.state);
+    // add the way to edit toDo
+    // checked not working when there is more than one toDO
+    const toDos = this.state.toDos.map((toDo, i) => {
+      return (
+        <div key={i}>
+          <input name="isDone" type="checkbox" checked={this.state.isDone} onChange={this.handleCheckChange}></input>
+          <span style={{ textDecoration: this.state.isDone ? 'line-through' : 'none'}}>{toDo}</span>
+          <button onClick={() => this.deleteToDo(toDo)}>Delete</button>
+          <button>Edit</button>
+          <input value={toDo} hidden></input>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+        <input type="text" value={this.state.toDo} onChange={this.handleOnChange}></input>
+        <button onClick={this.addToDo}>Add To Do</button>
+        {toDos}
+      </div>
+    );
+  }
 }
 
 export default App;
