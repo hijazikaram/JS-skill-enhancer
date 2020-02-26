6bb625752fa1ec13866d94052568cd8d4be3266b
Karam Hijazi <khijazi@Karams-MBP.fios-router.home>
2020-02-25 19:38:33 -0500

    working on toy problems

---
+++ toy-problems/add-nums-from-single-num.js
@@ -0,0 +1,21 @@
+// Easy Toy Problem
+// Create a function that takes a number as an argument.
+// Add up all the numbers from 1 to the number you passed to the function. 
+// For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+
+function addUp(num) {
+    let zero = 0;
+    for (let index = 1; index <= num; index++) {
+        zero = zero + index;
+    }
+    return zero
+}
+
+console.log(addUp(600))
---
+++ toy-problems/sum-of-two-nums.js
@@ -0,0 +1,6 @@
+// Easy
+// Add 2 nums
+function addition(a, b) { 
+    return a + b
+} 
+console.log(addition(-6, -2)) 
