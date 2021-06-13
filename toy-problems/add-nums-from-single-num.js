27b32292d5411b4205789216ffb18d9fdcb0deef
Essa Hijazi <essahijazi18@gmail.com>
2021-06-13 19:27:38 -0400

    Test

--- toy-problems/add-nums-from-single-num.js
+++ toy-problems/add-nums-from-single-num.js
@@ -1,6 +1,7 @@
+// test
 // Easy Toy Problem
 // Create a function that takes a number as an argument.
-// Add up all the numbers from 1 to the number you passed to the function. 
+// Add up all the numbers from 1 to the number you passed to the function.
 // For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
 
 // Examples
@@ -11,11 +12,11 @@
 // addUp(600) ➞ 180300
 
 function addUp(num) {
-    let zero = 0;
-    for (let index = 1; index <= num; index++) {
-        zero = zero + index;
-    };
-    return zero;
+  let zero = 0;
+  for (let index = 1; index <= num; index++) {
+    zero = zero + index;
+  }
+  return zero;
 }
 
-console.log(addUp(10))
+console.log(addUp(10));
